2d1513b22aba50df9aa2c0c68b04ef96d64f4a3b
shubhammittal <shubhammittal2020@gmail.com>
2023-08-30 11:42:29 +0530

    Set and get data

--- Assets/Scripts/TemplateManager.cs
+++ Assets/Scripts/TemplateManager.cs
@@ -1,55 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using TMPro;
+using UnityEngine.UIElements.Experimental;
+
+public class ImageTemplate {
+    public string name;
+    public Vector3 position;
+    public Quaternion rotation;
+    public Texture texture;
+
+    //public ImageTemplate() { }
+
+    public ImageTemplate(string name, Vector3 position, Quaternion rotation, Texture texture) { 
+        this.name = name;
+        this.position = position;
+        this.rotation = rotation;
+        this.texture = texture;
+    }
+}
+
+public class TextTemplate {
+    public string name;
+    public Vector3 position;
+    public Quaternion rotation;
+    public string text;
+    public Color textColor;
+
+    //public TextTemplate() { }
+
+    public TextTemplate(string name, Vector3 position, Quaternion rotation, string text, Color textColor) {
+        this.name = name;
+        this.position = position;
+        this.rotation = rotation;
+        this.text = text;
+        this.textColor = textColor;
+    }
+}
 
 public class TemplateManager : MonoBehaviour {
 
+    public static TemplateManager Instance;
+
     [SerializeField] Transform parent;
+    [SerializeField] Texture sampleImage;
 
-    [SerializeField] Sprite sprite;
-
-    private string Heading = "This is my heading";
+    ImageTemplate image1 = new ImageTemplate(string.Empty, Vector3.zero, Quaternion.identity, null);
+    TextTemplate text1 = new TextTemplate(string.Empty, Vector3.zero, Quaternion.identity, "", Color.black);
 
     void Start() {
-        CreateSampleImage();
-        CreateSampleText();
+
+        SetImageData(image1);
+        SetTextData(text1);
+
+        CreateSampleImage(GetImageData(image1));
+        CreateSampleText(GetTextData(text1));
     }
 
-    void CreateSampleImage() { 
-        GameObject obj = new GameObject("Sample Image Template");
+    #region Image Template
 
-        // Set Parent
-        obj.AddComponent<RectTransform>();
+    void SetImageData(ImageTemplate image) {
+        // Manually assigning all values
+        image.name = "SampleImageTemplate";
+
+        image.position = Vector3.zero;
+        image.rotation = Quaternion.identity;
+
+        image.texture = sampleImage;
+    }
+
+
+    void CreateSampleImage(GameObject obj) {  
         obj.GetComponent<RectTransform>().SetParent(parent);
-        
-        // Set Position
-        obj.GetComponent<RectTransform>().localPosition = Vector3.zero;
-        obj.GetComponent<RectTransform>().localRotation = Quaternion.identity;
+    }
+
+    GameObject GetImageData(ImageTemplate image) {
+        GameObject obj = new GameObject(image.name);
+
+        // Add RectTransform
+        obj.AddComponent<RectTransform>();
+
+        // Set Transform
+        obj.GetComponent<RectTransform>().localPosition = image.position;
+        obj.GetComponent<RectTransform>().localRotation = image.rotation;
         obj.GetComponent<RectTransform>().localScale = Vector3.one;
 
         // Attach Image
         obj.AddComponent<RawImage>();
-        obj.GetComponent<RawImage>().texture = sprite.texture;        
-    }
-    
-    void CreateSampleText() { 
-        GameObject obj = new GameObject("Sample Text Template");
 
-        // Set Parent
+        // Set Image
+        obj.GetComponent<RawImage>().texture = image.texture;
+
+        return obj;
+    }
+
+    #endregion
+
+    #region Text Template
+
+    void SetTextData(TextTemplate myText) {
+        // Manually assigning all values
+        myText.name = "SampleTextTemplate";
+
+        myText.position = Vector3.zero;
+        myText.rotation = Quaternion.identity;
+
+        myText.text = "This is the sample Text";
+        myText.textColor = Color.black;
+    }
+
+    GameObject GetTextData(TextTemplate myText) {
+        GameObject obj = new GameObject(myText.name);
+
+        // Add RectTransform
         obj.AddComponent<RectTransform>();
-        obj.GetComponent<RectTransform>().SetParent(parent);
-        
-        // Set Position
-        obj.GetComponent<RectTransform>().localPosition = Vector3.zero;
-        obj.GetComponent<RectTransform>().localRotation = Quaternion.identity;
+
+        // Set Transform
+        obj.GetComponent<RectTransform>().localPosition = myText.position;
+        obj.GetComponent<RectTransform>().localRotation = myText.rotation;
         obj.GetComponent<RectTransform>().localScale = Vector3.one;
 
-        // Attach Text
+        // Add TextMeshProUGUI
         obj.AddComponent<TextMeshProUGUI>();
-        obj.GetComponent<TextMeshProUGUI>().text = Heading;
+
+        // Set Text
+        obj.GetComponent<TextMeshProUGUI>().text = myText.text;
         obj.GetComponent<TextMeshProUGUI>().enableAutoSizing = true;
-        obj.GetComponent<TextMeshProUGUI>().color = Color.black;
+        obj.GetComponent<TextMeshProUGUI>().color = myText.textColor;
         obj.GetComponent<TextMeshProUGUI>().alignment = TextAlignmentOptions.Center;
+
+        return obj;
     }
+
+    void CreateSampleText(GameObject obj) {
+        obj.GetComponent<RectTransform>().SetParent(parent);
+    }
+
+    #endregion
 
 }
